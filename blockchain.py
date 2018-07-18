a3e90dae1a065b9f3bf4f202ac1c7d9df21285ab
Akash Sengar <akashsengar96@gmail.com>
2018-07-18 07:39:43 +0530

    Changes

--- blockchain.py
+++ blockchain.py
@@ -8,11 +8,11 @@ def add_value(txn_amount,last_tranx = [1]):
     blockchain.append([last_tranx,txn_amount])
 
 def get_user_input():
-    return float(input('Your Transation value : '))
+    return float(input('Enter your Transaction Value : '))
 
 
 txn_amount = get_user_input()
-add_value(txn_amount)
+add_value(txn_amount)   
 txn_amount =get_user_input()
 add_value(txn_amount,get_last_blockchain_value())
 txn_amount = get_user_input()
